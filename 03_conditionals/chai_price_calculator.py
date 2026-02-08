2e85f44f3b039fb1d38b0a97a29598ee6148f127
Prakash Raj <singhprakash2742003@gmail.com>
2026-02-08 16:45:12 +0530

    3_conditionals

---
+++ 03_conditionals/chai_price_calculator.py
@@ -0,0 +1,10 @@
+cup_size = input("Enter the cup size: ").lower() 
+
+if cup_size == 'small':
+    print("Price is 10 rupees") 
+elif cup_size == 'medium':
+    print("Price is 15 rupees") 
+elif cup_size == 'large':
+    print("Price is 20 rupees") 
+else:
+    print("Unknown cup size")
---
+++ 03_conditionals/mini_story_1.py
@@ -0,0 +1,4 @@
+kettle_variable = True 
+
+if kettle_variable:
+    print("Kettle done! Time to make chai!")
